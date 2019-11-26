420206c84d3e4c2afe0ad1097c647d4fdb2d901a
kaneki666 <41444793+kaneki666@users.noreply.github.com>
2019-11-26 13:16:45 +0600

    edit

--- src/components/Search.js
+++ src/components/Search.js
@@ -57,10 +57,10 @@ class Search extends React.Component {
 			<div className="results-container">
 				{results.map((result) => {
 					return (
-						<a key={result.id} href={result.previewURL} className="result-items">
+						<a key={result.id} href={result.largeImageURL} className="result-items">
 							<h6 className="image-username">{result.user}</h6>
 							<div className="image-wrapper">
-								<img className="image" src={result.previewURL} alt={result.user}/>
+								<img className="image" src={result.webformatURL} alt={result.user}/>
 							</div>
 						</a>
 					);
